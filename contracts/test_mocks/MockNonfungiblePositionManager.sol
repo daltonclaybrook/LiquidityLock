8064d11efa6a8f327d16fefdb79f584a1630ab0a
Dalton Claybrook <daltonclaybrook@gmail.com>
2022-02-27 13:52:22 -0500

    Add tests

--- contracts/test_mocks/MockNonfungiblePositionManager.sol
+++ contracts/test_mocks/MockNonfungiblePositionManager.sol
@@ -8,8 +8,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockNonfungiblePositionManager is INonfungiblePositionManager, ERC721 {
     uint256 private _nextId = 1;
-    MockToken private _token0;
-    MockToken private _token1;
+    MockToken public token0;
+    MockToken public token1;
     
     mapping(uint256 => Position) private _positions;
 
@@ -19,8 +19,8 @@ contract MockNonfungiblePositionManager is INonfungiblePositionManager, ERC721 {
     }
 
     constructor(uint256 mintTokens) ERC721("MockPositionManager", "MPM") {
-        _token0 = new MockToken("Mock Token 0", "MT0", mintTokens);
-        _token1 = new MockToken("Mock Token 1", "MT1", mintTokens);
+        token0 = new MockToken("Mock Token 0", "MT0", mintTokens);
+        token1 = new MockToken("Mock Token 1", "MT1", mintTokens);
         createMockPosition();
     }
 
@@ -29,8 +29,8 @@ contract MockNonfungiblePositionManager is INonfungiblePositionManager, ERC721 {
     function positions(uint256 tokenId) external view returns (
         uint96 nonce,
         address operator,
-        address token0,
-        address token1,
+        address _token0,
+        address _token1,
         uint24 fee,
         int24 tickLower,
         int24 tickUpper,
@@ -45,8 +45,8 @@ contract MockNonfungiblePositionManager is INonfungiblePositionManager, ERC721 {
 
         nonce = 0; // unused
         operator = position.owner;
-        token0 = address(_token0);
-        token1 = address(_token1);
+        _token0 = address(token0);
+        _token1 = address(token1);
         fee = 3000; // unused
         tickLower = 0; // unused
         tickUpper = 0; // unused
@@ -65,8 +65,8 @@ contract MockNonfungiblePositionManager is INonfungiblePositionManager, ERC721 {
         (uint256 balance0, uint256 balance1) = tokenBalances();
         require(balance0 >= params.liquidity && balance1 >= params.liquidity, "Balance too low");
 
-        transferToken(_token0, position.owner, params.liquidity);
-        transferToken(_token1, position.owner, params.liquidity);
+        transferToken(token0, position.owner, params.liquidity);
+        transferToken(token1, position.owner, params.liquidity);
         position.liquidity -= params.liquidity;
 
         return (params.liquidity, params.liquidity);
@@ -87,8 +87,8 @@ contract MockNonfungiblePositionManager is INonfungiblePositionManager, ERC721 {
         // after the position was created, simulating a fee.
         amount0 = balance0 - position.liquidity;
         amount1 = balance1 - position.liquidity;
-        transferToken(_token0, position.owner, amount0);
-        transferToken(_token1, position.owner, amount1);
+        transferToken(token0, position.owner, amount0);
+        transferToken(token1, position.owner, amount1);
     }
 
     // MARK: - Mock helper functions
@@ -106,8 +106,8 @@ contract MockNonfungiblePositionManager is INonfungiblePositionManager, ERC721 {
     }
 
     function tokenBalances() private view returns (uint256 token0Balance, uint256 token1Balance) {
-        token0Balance = _token0.balanceOf(address(this));
-        token1Balance = _token1.balanceOf(address(this));
+        token0Balance = token0.balanceOf(address(this));
+        token1Balance = token1.balanceOf(address(this));
     }
 
     function transferToken(MockToken token, address to, uint256 amount) private {
--- test/liquidity_lock_tests.js
+++ test/liquidity_lock_tests.js
@@ -1,7 +1,9 @@
 const LiquidityLock = artifacts.require("LiquidityLock");
 const PositionManager = artifacts.require("MockNonfungiblePositionManager");
+const MockToken = artifacts.require("MockToken");
+const BN = web3.utils.BN;
 
-contract("LiquidityLock", (/* accounts */) => {
+contract("LiquidityLock", (accounts) => {
     it("has correct name and symbol", async () => {
         const lock = await LiquidityLock.deployed();
 
@@ -11,9 +13,37 @@ contract("LiquidityLock", (/* accounts */) => {
         assert.equal(symbol, "UV3LL");
     });
 
-    it("has deployed mock contract", async () => {
-        const manager = await PositionManager.deployed();
-        const name = await manager.name();
-        assert.equal(name, "MockPositionManager");
+    describe("mock setup", () => {
+        it("has deployed mock contract", async () => {
+            const manager = await PositionManager.deployed();
+            const name = await manager.name();
+            assert.equal(name, "MockPositionManager");
+        });
+
+        it("deploys two unique mock tokens", async () => {
+            const manager = await PositionManager.deployed();
+            const token0 = await MockToken.at(await manager.token0());
+            const token1 = await MockToken.at(await manager.token1());
+
+            assert.notEqual(token0.address, token1.address);
+            assert.equal(token0.address.length, 42);
+            assert.equal(token1.address.length, 42);
+
+            assert.equal(await token0.name(), "Mock Token 0");
+            assert.equal(await token1.name(), "Mock Token 1");
+            assert.equal(await token0.symbol(), "MT0");
+            assert.equal(await token1.symbol(), "MT1");
+        });
+
+        it("mints one million of each mock token to manager", async () => {
+            const manager = await PositionManager.deployed();
+            const token0 = await MockToken.at(await manager.token0());
+            const token1 = await MockToken.at(await manager.token1());
+
+            const balance0 = await token0.balanceOf(manager.address);
+            const balance1 = await token1.balanceOf(manager.address);
+            assert.equal(balance0.toString(), "1000000");
+            assert.equal(balance1.toString(), "1000000");
+        });
     });
 });
